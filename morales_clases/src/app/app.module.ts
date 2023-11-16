f8360778cabaeeb95e117f0223393f31611a196d
12CUY <wiliampuntomorales45@gmail.com>
2023-11-16 15:32:43 -0500

    uso de formulario reactivo

--- morales_clases/src/app/app.module.ts
+++ morales_clases/src/app/app.module.ts
@@ -2,15 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import { UserComponent } from './pages/user/user.component';
 
-const routes: Routes = [
-  // Otras rutas aquí...
-  { path: '', redirectTo: '/usuario', pathMatch: 'full' },
-  { path: 'usuario', component: UserComponent }
-];
 
 @NgModule({
   declarations: [
@@ -20,7 +14,8 @@ const routes: Routes = [
   imports: [
     FormsModule,
     BrowserModule,
-    RouterModule.forRoot(routes)  // Configuración de las rutas principales
+    FormBuilder,
+    FormGroup
   ],
   providers: [],
   bootstrap: [AppComponent]
--- morales_clases/src/app/pages/user/user.component.ts
+++ morales_clases/src/app/pages/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { user } from 'src/app/models/user.interface';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -7,45 +7,17 @@ import { user } from 'src/app/models/user.interface';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
-    // declaracion de variable
-    people:user;
-    person:any;
-
-    constructor (){
-        // incializacion de la variable de "people"
-      this.people={
-        name : "wiliam",
-        age :2,
-        genderId : 1,
-        genderName : ""
-      }
-
-    // declaracion de un array people con un objeto
-    const people=[
-    {
-      name : "wiliam",
-      age :2,
-      genderId : 1,
-      genderName : ""
-    }
-
-    ]
-    // uso del array map 
-    //transformar el objeto people
-  this.person = people.map(person => {
-
-      if(person.genderId==1){
-          person.genderName="sr"
-        }
-        else{
-          person.genderName="sra"
-        }
-
-        return person;
 
 
-  })
- // imprimie la consola el resultado
-  console.log(this.person);
-  }
+protected form:FormGroup;
+
+constructor(private formBuilder: FormBuilder) {
+  this.form = this.formBuilder.group({
+    name: [null, [Validators.required, Validators.minLength(3)]], 
+    age: [null, [Validators.required, Validators.minLength(3)]], 
+    genderId: [null, [Validators.required, Validators.minLength(3)]], 
+    genderName: [null, [Validators.required, Validators.minLength(3)]]
+  });
+}
+
 }
